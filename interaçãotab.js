f2e587c578aa5720629940a6dfcd7c9b4a321a72
Bruno Dayron <109740548+DayronTi@users.noreply.github.com>
2023-06-21 23:19:06 -0300

    JavaScript

---
+++ interaçãotab.js
@@ -0,0 +1,13 @@
+function tabuada() {
+    let num = document.getElementById('txtn')               //variavel num ligada com o elemento do documento identificado por txtn
+    let tab = documnet.getElementById('seltab')             //variavel tab ligada com o elemento do documento identificado por seltab
+    if (num == 0) {                             //validacao: se o valor de num for 0 ou vazio escreva um alerta, se nao continue
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)                               //variavel n recebe o valor numerico de num
+    }
+
+
+
+
+}
